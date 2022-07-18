999ea9fe3f9bd05d2e66552e7e346f1da652b521
Qingying Meng <qmeng222@gmail.com>
2022-07-18 14:06:57 -0700

    books added

--- books_app/models.py
+++ books_app/models.py
@@ -3,15 +3,15 @@ from django.db import models
 # Create your models here.
 
 class Book(models.Model):
-   title = models.CharField(max_length=125)
-   author = models.CharField(max_length=100)
-   pages = models.IntegerField()
-   isbn = models.IntegerField()
-   book_cover_url = models.URLField(null=True, blank=True)
-   is_in_print = models.BooleanField()
+   title = models.CharField(max_length=200, unique=True)
+   author = models.CharField(max_length=200)
+   pages = models.SmallIntegerField(null=True)
+   isbn = models.BigIntegerField(null=True)
+   is_in_print = models.BooleanField(null=True)
    pub_date = models.DateField()
-   # description = models.TextField()
+   book_cover_url = models.URLField(null=True, blank=True)
+   description = models.TextField(null=True)
    # imagef = models.URLField(null=True, blank=True)
 
    def __str__(self):
-       return self.name + ' by ' + self. author
+       return self.title + ' by ' + self. author
