dcd154c23595af72c9c75c14294120901396f910
Qingying Meng <qmeng222@gmail.com>
2022-07-20 10:02:00 -0700

    v.4, LUB  7/20/2022

--- books_app/urls.py
+++ books_app/urls.py
@@ -15,8 +15,7 @@ urlpatterns = [
     path("create/", create_book,  name="create_book"),
     path("", show_books, name="show_books"),
     path("<int:pk>/", show_book, name="book_detail"),
-    # path("new/", create_book, name="book_new"),
-    # path("edit/", edit_book, name="book_edit"),
+    path("edit/", edit_book, name="book_edit"),
 ]
 
 
